55eed9e77873d27e08c3c452490af2cfb9ab41b7
Den-Git-CPP <madim-neodart@rambler.ru>
2022-04-02 16:23:55 +0300

    with cout build N

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "lib.h"
+
+#include <iostream>
+
+using namespace std;
+
+int main (int, char **)
+{
+    cout << "build " << version() << endl;
+    cout << "Hello, World!" << endl;
+
+    return 0;
+}
